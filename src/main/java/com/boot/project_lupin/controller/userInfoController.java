f79e6fe7fe90c6e0363c5ed6fda1c3a9a59c5da0
dsafj112 <fgghk132@naver.com>
2024-09-20 10:09:00 +0900

    240920 나성엽(유저 주소 변경)

--- src/main/java/com/boot/project_lupin/controller/userInfoController.java
+++ src/main/java/com/boot/project_lupin/controller/userInfoController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,7 +20,7 @@ public class userInfoController {
 	private UserInfoService service;
 
 	@RequestMapping("/userInfo")
-	public String question(HttpServletRequest httpServletRequest, Model model) {
+	public String userInfo(HttpServletRequest httpServletRequest, Model model) {
 		log.info("@# userInfo");
 
 		// HttpSession에서 사용자 정보 가져오기
@@ -38,6 +39,20 @@ public class userInfoController {
 		}
 
 		return "userInfo";
+	}
+
+
+	@PostMapping("/updateInfo")
+	public String updateInfo(UserInfoDTO userInfoDTO, HttpServletRequest httpServletRequest) {
+		log.info("@# updateInfo");
+
+		// 주소 업데이트 서비스 호출
+		service.updateAddress(userInfoDTO);
+
+		// 알림 메시지 설정
+		httpServletRequest.setAttribute("msg", "주소를 변경하였습니다.");
+		httpServletRequest.setAttribute("url", "/userInfo");
+		return "/alert";
 	}
 
 }
--- src/main/java/com/boot/project_lupin/dao/UserInfoDAO.java
+++ src/main/java/com/boot/project_lupin/dao/UserInfoDAO.java
@@ -8,4 +8,6 @@ public interface UserInfoDAO {
 
 	public UserInfoDTO selectUserInfo(String username); // 로그인된 유저 정보 가져옴
 
+	public void updateAddress(UserInfoDTO userInfoDTO); // 유저 주소 업데이트
+
 }
